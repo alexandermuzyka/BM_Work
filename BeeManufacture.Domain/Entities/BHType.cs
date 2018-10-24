53a25440b80b2f767becb3c2a95c14ceb988d231
Oleksandr_work <a.muzyka@Upitec.com>
2018-10-24 10:54:19 +0300

    First commit

---
+++ BeeManufacture.Domain/Entities/BHType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeeManufacture.Domain.Entities
+{
+    public class BHType
+    {
+        [Key]
+        public int BHTypeId { get; set; }
+        public string Name { get; set; }
+        public int QuantityOnManufacture { get; set; }
+    }
+}
